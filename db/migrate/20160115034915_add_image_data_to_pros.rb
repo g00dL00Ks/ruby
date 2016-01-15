f91564fd4c513f30345151a614cbee1fdab57e4c
Braden Perez-Mugg <braden.mugg@gmail.com>
2016-01-14 23:02:39 -0500

    Removed extra migration

--- db/migrate/20160115034915_add_image_data_to_pros.rb
+++
@@ -1,8 +0,0 @@
-class AddImageDataToPros < ActiveRecord::Migration
-  def change
-    add_column :pros, :image_file_name, :string
-    add_column :pros, :image_content_type, :string
-    add_column :pros, :image_file_size, :integer
-    add_column :pros, :image_updated_at, :datetime
-  end
-end
