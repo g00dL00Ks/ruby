5deda64f4649e7a1c0713837e83ee3bd09c7228e
Nolan Zandi <nolan@zandi.com>
2016-01-18 05:18:31 -0800

    pro-photo-gallery-first-draft

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,6 @@
 class PhotosController < ApplicationController
-  #before_action :set_pro
+  before_action :set_photo, only: [:show, :edit, :update, :destroy]
+  before_action :set_pro
   before_filter :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
@@ -7,13 +8,13 @@ class PhotosController < ApplicationController
   end
 
   def new
-    @pro = current_user.pro
-    @photo = @pro.photos.build
+    @photo = Photo.new
   end
 
   def create
-    @pro = current_user.pro
-    @photo = @pro.photos.build
+    @photo = Photo.new(photo_params)
+    @photo.pro_id = current_user.pro.id
+    Aws.use_bundled_cert!
     @photo.save
     respond_to do |format|
       if @photo.save
@@ -28,7 +29,15 @@ class PhotosController < ApplicationController
 
   private
 
+  def set_photo
+    @photo = Photo.find(params[:pro_id])
+  end
+
+  def set_pro
+    @pro = Pro.find(params[:pro_id])
+  end
+
   def photo_params
-    params.require(:photo).permit(:image, :title, :pro_id, :url, :path)
+    params.require(:photo).permit!
   end
 end
--- app/controllers/pros_controller.rb
+++ app/controllers/pros_controller.rb
@@ -92,6 +92,6 @@ class ProsController < ApplicationController
         :exec_coaching, :lifestyle_coaching, :public_speaking, :dating, :time_mgmt, :ADD_coaching, 
         :stress_mgmt, :parenting, :spirituality, :coach_bundle, :coach_other, :q_enjoy, :q_approach, :q_common,
         service_attributes: [:title, :description, :length, :price, :pro_id],
-        photo_attributes: [:image, :title, :pro_id, :url, :path])
+        photo_attributes: [:image, :title])
     end
 end
